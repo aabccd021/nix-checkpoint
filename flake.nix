7e7357f1e5c87299b4949b53e46622c042966029
aabccd021 <aabccd021@gmail.com>
2025-01-20 11:18:30 +0700

    Commit message: "Implement checkpoint script and flake.lock and flake.nix files

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    aicommit = {
+      url = "github:nguyenvanduocit/ai-commit";
+      flake = false;
+    };
+  };
+
+  outputs = { nixpkgs, aicommit, treefmt-nix, self }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      aicommitPkgs = pkgs.buildGoModule {
+        name = "ai-commit";
+        src = aicommit;
+        vendorHash = "sha256-BPxPonschTe8sWc5pATAJuxpn7dgRBeVZQMHUJKpmTk=";
+      };
+
+      checkpoint = pkgs.writeShellApplication {
+        name = "checkpoint";
+        runtimeInputs = [ aicommitPkgs pkgs.findutils ];
+        text = builtins.readFile ./checkpoint.sh;
+      };
+
+
+      packages = {
+        checkpoint = checkpoint;
+        formatting = treefmtEval.config.build.check self;
+        snapshot-test = pkgs.runCommandNoCCLocal "snapshot-test" { } ''
+          mkdir -p $out/snapshot/nested
+          echo "foo" > $out/snapshot/nested/file.txt
+        '';
+      };
+
+      gcroot = packages // {
+        gcroot-all = pkgs.linkFarm "gcroot-all" packages;
+      };
+
+      treefmtEval = treefmt-nix.lib.evalModule pkgs {
+        projectRootFile = "flake.nix";
+        programs.nixpkgs-fmt.enable = true;
+        programs.prettier.enable = true;
+        programs.shfmt.enable = true;
+        programs.shellcheck.enable = true;
+        settings.formatter.shellcheck.options = [ "-s" "sh" ];
+      };
+
+    in
+
+    {
+
+      devShells.x86_64-linux.default = pkgs.mkShellNoCC {
+        buildInputs = [
+          checkpoint
+        ];
+      };
+
+      packages.x86_64-linux = gcroot;
+
+      checks.x86_64-linux = gcroot;
+
+      formatter.x86_64-linux = treefmtEval.config.build.wrapper;
+
+      apps.x86_64-linux = {
+        checkpoint-fix = {
+          type = "app";
+          program = toString (
+            pkgs.writeShellScript "checkpoint-fix" ''
+              echo "running checkpoint-fix"
+              sleep 1
+              echo "done checkpoint-fix"
+            ''
+          );
+        };
+      };
+
+    };
+}
