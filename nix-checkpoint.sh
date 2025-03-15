4fbc935d324a252e28c876f6cac1e3dc3593785e
aabccd021 <aabccd021@gmail.com>
2025-03-15 14:22:52 +0700

    Refactor nix-checkpoint.sh script - remove gcroots processing.

--- nix-checkpoint.sh
+++ nix-checkpoint.sh
@@ -135,9 +135,7 @@ if [ -n "$(echo "$packages" | grep '^gcroot$' || true)" ]; then
   rm -rf .gcroot
   mkdir -p .gcroot
   branch_name=$(git branch --show-current)
-  for gcroot in $gcroots; do
-    nohup nix build --out-link ".gcroot/$branch_name" .#gcroot </dev/null >/dev/null 2>&1 &
-  done
+  # nohup nix build --out-link ".gcroot/$branch_name" .#gcroot </dev/null >/dev/null 2>&1 &
 
   # delete if there is gcroots for branch that doesn't exist locally anymore
   set -x
