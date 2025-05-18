6df6e5019eb3ba17c1c7e4bfca65a10132f281e4
aabccd021 <aabccd021@gmail.com>
2025-05-19 06:15:22 +0700

    Revised nix-checkpoint script for improved readability

--- nix-checkpoint.sh
+++ nix-checkpoint.sh
@@ -1,5 +1,3 @@
-flag=${1:-}
-
 root=$(git rev-parse --show-toplevel)
 trap 'cd $(pwd)' EXIT
 cd "$root" || exit
@@ -7,9 +5,10 @@ git add --all >/dev/null
 
 system=$(nix eval --impure --raw --expr 'builtins.currentSystem')
 
+printf "nix flake show --json"
 start=$(date +%s)
 flake_details=$(nix flake show --json)
-echo "nix flake show finished successfully in $(($(date +%s) - start))s"
+echo " ($(($(date +%s) - start))s)"
 
 packages=$(
   echo "$flake_details" |
@@ -17,37 +16,27 @@ packages=$(
     true
 )
 
-create_snapshot() {
-  start=$(date +%s)
-  result=$(nix build --no-link --print-out-paths ".#$1")
-  files=$(find -L "$result" -type f -printf '%P\n')
-  for file in $files; do
-    mkdir -p "$(dirname "$file")"
-    cp -L "$result/$file" "$file"
-    chmod 644 "$file"
-  done
-  echo "$snapshot created successfully in $(($(date +%s) - start))s"
-}
-
 snapshots=$(echo "$packages" | grep '^snapshot-' || true)
 if [ -n "$snapshots" ]; then
-  pids=""
   for snapshot in $snapshots; do
-    create_snapshot "$snapshot" &
-    pids="$pids $!"
-  done
-  for pid in $pids; do
-    wait "$pid" || exit 1
-  done
-fi
 
-if [ "$flag" = "--snapshot" ]; then
-  git reset >/dev/null
-  exit 0
+    printf "nix build --no-link --print-out-paths .#%s" "$snapshot"
+    start=$(date +%s)
+    result=$(nix build --no-link --print-out-paths ".#$snapshot")
+    echo " ($(($(date +%s) - start))s)"
+
+    files=$(find -L "$result" -type f -printf '%P\n')
+    for file in $files; do
+      mkdir -p "$(dirname "$file")"
+      cp -L "$result/$file" "$file"
+      chmod 644 "$file"
+    done
+  done
 fi
 
 new_files=$(git diff --cached --name-only --diff-filter=A)
-if [ -n "$new_files" ]; then
+new_files_hashed=$(echo "$new_files" | md5sum | cut -d' ' -f1)
+if [ -n "$new_files" ] && [ ! -f "/tmp/$new_files_hashed" ]; then
   echo "New file(s) detected!"
   echo
   echo "$new_files"
@@ -60,13 +49,13 @@ if [ -n "$new_files" ]; then
     git reset >/dev/null
     exit 1
   fi
+  touch "/tmp/$new_files_hashed"
 fi
 
+printf "git add --all"
+start=$(date +%s)
 git add --all >/dev/null
-
-if [ "$flag" = "--no-fix" ]; then
-  exit 0
-fi
+echo " ($(($(date +%s) - start))s)"
 
 fix_apps=$(
   echo "$flake_details" |
@@ -79,64 +68,48 @@ fix_packages=$(
     true
 )
 if [ -n "$fix_apps" ] || [ -n "$fix_packages" ]; then
+  printf "nix run .#fix"
   start=$(date +%s)
   nix run ".#fix"
-  echo "nix run .#fix finished successfully in $(($(date +%s) - start))s"
-fi
-
-if [ "$flag" = "--fix" ] || [ "$flag" = "--no-fmt" ]; then
-  git reset >/dev/null
-  exit 0
+  echo " ($(($(date +%s) - start))s)"
 fi
 
 has_formatter=$(echo "$flake_details" |
   jq ".formatter[\"$system\"]" 2>/dev/null || true)
 if [ -n "$has_formatter" ] && [ "$has_formatter" != "null" ]; then
+  printf "nix fmt"
   start=$(date +%s)
   nix fmt
-  echo "nix fmt finished successfully in $(($(date +%s) - start))s"
+  echo " ($(($(date +%s) - start))s)"
 fi
 
-if [ "$flag" = "--fmt" ] || [ "$flag" = "--no-check" ]; then
-  git reset >/dev/null
-  exit 0
-fi
-
-if [ "$flag" != "--skip-check" ]; then
-
-  start=$(date +%s)
-  git add --all >/dev/null
-  nix flake check --quiet || (git reset >/dev/null && exit 1)
-  echo "nix flake check finished successfully in $(($(date +%s) - start))s"
-
-fi
-
-if [ "$flag" = "--check" ] || [ "$flag" = "--no-commit" ]; then
-  git reset >/dev/null
-  exit 0
-fi
-
+printf "git add --all"
 start=$(date +%s)
+git add --all >/dev/null
+echo " ($(($(date +%s) - start))s)"
 
+printf "nix flake check --quiet"
+start=$(date +%s)
+nix flake check --quiet || (git reset >/dev/null && exit 1)
+echo " ($(($(date +%s) - start))s)"
+
+printf "ai-commit --auto-commit"
+start=$(date +%s)
 timeout 10 ai-commit --auto-commit >/dev/null 2>&1 ||
   git commit --edit --message "checkpoint"
+echo " ($(($(date +%s) - start))s)"
 
-echo "Commit message generated successfully in $(($(date +%s) - start))s"
+rm "/tmp/$new_files_hashed" 2>/dev/null || true
 
-if [ "$flag" = "--commit" ] || [ "$flag" = "--no-push" ]; then
-  exit 0
-fi
-
+printf "git pull --quiet --rebase"
 start=$(date +%s)
-
 git pull --quiet --rebase
+echo " ($(($(date +%s) - start))s)"
+
+printf "git push --quiet"
+start=$(date +%s)
 git push --quiet
-
-echo "Respository pushed successfully in $(($(date +%s) - start))s"
-
-if [ "$flag" = "--push" ] || [ "$flag" = "--no-gcroot" ]; then
-  exit 0
-fi
+echo " ($(($(date +%s) - start))s)"
 
 gcroot_exists=$(
   echo "$flake_details" |
@@ -144,7 +117,11 @@ gcroot_exists=$(
 )
 if [ "$gcroot_exists" = "true" ]; then
   rm -rf .gcroot
-  nohup nix build --out-link .gcroot .#gcroot </dev/null >/dev/null 2>&1 &
+
+  printf "nix build --out-link .gcroot .#gcroot"
+  start=$(date +%s)
+  nix build --out-link .gcroot .#gcroot
+  echo " ($(($(date +%s) - start))s)"
 fi
 
 if command -v notify-send >/dev/null 2>&1; then
